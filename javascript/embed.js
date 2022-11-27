9fe2cf1560a8b218b0c82691c6a628e20d38808d
ruaa-1982 <ruaa1982alagha@gmail.com>
2022-11-27 22:53:00 +0100

    mycv

---
+++ javascript/embed.js
@@ -0,0 +1,7 @@
+let mycontainer= document.querySelector(".container");
+let myembed= document.createElement("embed");
+myembed.className="myembedpdf";
+myembed.src="./file/JD-CV Ruaa ALMURAD AGHA.pdf";
+myembed.type="application/pdf";
+mycontainer.appendChild(myembed);
+
